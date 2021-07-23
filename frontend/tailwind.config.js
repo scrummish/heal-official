2ffb9da0f5476dba6313819a294e061939dff894
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-07-23 18:44:49 -0500

    Added all colors freom style guide

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,42 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: colors.indigo,
+        primary: colors.purple,
+        purple: {
+          light: "#c0b3c5",
+          DEFAULT: "#532565",
+          dark: "",
+        },
+        magenta: {
+          light: "#dabeca",
+          DEFAULT: "#982568",
+          dark: "",
+        },
+        red: {
+          light: "#dbb9b3",
+          DEFAULT: "#981f32",
+          dark: "",
+        },
+        coral: {
+          light: "#eecdcb",
+          DEFAULT: "#bf362e",
+          dark: "",
+        },
+        orange: {
+          light: "f5d8c3",
+          DEFAULT: "#e07c3e",
+          dark: "",
+        },
+        gray: {
+          light: "#dddddd",
+          DEFAULT: "#818a91",
+          dark: "#373a3c",
+        },
+        blue: {
+          light: "",
+          DEFAULT: "#0044B3",
+          dark: "",
+        },
       },
       container: {
         center: true,
