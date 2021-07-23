f0a92bb43100f7d43e3a353d9a558bb9a1361bea
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-07-23 17:19:50 -0500

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0485586296a6572ee4ad5592480ea7c8'),
+    },
+  },
+});
