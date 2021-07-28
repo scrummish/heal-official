470eecbe6203464ce6395b21e5c19fcac41460b4
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-07-27 21:53:23 -0500

    Contact form now collects names

--- frontend/components/sections/lead-form.js
+++ frontend/components/sections/lead-form.js
@@ -1,46 +1,57 @@
-import { useState } from "react"
-import { fetchAPI } from "utils/api"
-import * as yup from "yup"
-import { Formik, Form, Field } from "formik"
-import Button from "../elements/button"
+import { useState } from "react";
+import { fetchAPI } from "utils/api";
+import * as yup from "yup";
+import { Formik, Form, Field } from "formik";
+import Button from "../elements/button";
 
 const LeadForm = ({ data }) => {
-  const [loading, setLoading] = useState(false)
+  console.log("data");
+  console.log(data);
+  console.log("data");
+  const [loading, setLoading] = useState(false);
 
   const LeadSchema = yup.object().shape({
     email: yup.string().email().required(),
-  })
+    name: yup.string().required(),
+  });
 
   return (
     <div className="py-10 text-center">
       <h1 className="text-3xl mb-10 font-bold mb-2">{data.title}</h1>
       <div className="flex flex-col items-center">
         <Formik
-          initialValues={{ email: "" }}
+          initialValues={{ email: "", name: "" }}
           validationSchema={LeadSchema}
           onSubmit={async (values, { setSubmitting, setErrors }) => {
-            setLoading(true)
+            setLoading(true);
 
             try {
-              setErrors({ api: null })
+              setErrors({ api: null });
               await fetchAPI("/lead-form-submissions", {
                 method: "POST",
                 body: JSON.stringify({
                   email: values.email,
+                  name: values.name,
                   location: data.location,
                 }),
-              })
+              });
             } catch (err) {
-              setErrors({ api: err.message })
+              setErrors({ api: err.message });
             }
 
-            setLoading(false)
-            setSubmitting(false)
+            setLoading(false);
+            setSubmitting(false);
           }}
         >
           {({ errors, touched, isSubmitting }) => (
             <div>
               <Form className="flex flex-col md:flex-row gap-4">
+                <Field
+                  className="text-base focus:outline-none py-4 md:py-0 px-4 border-2 rounded-md"
+                  type="name"
+                  name="name"
+                  placeholder={data.name}
+                />
                 <Field
                   className="text-base focus:outline-none py-4 md:py-0 px-4 border-2 rounded-md"
                   type="email"
@@ -62,7 +73,7 @@ const LeadForm = ({ data }) => {
         </Formik>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeadForm
+export default LeadForm;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,7 +4,7 @@ import ErrorPage from "next/error";
 import { useRouter } from "next/router";
 import { DefaultSeo } from "next-seo";
 import { getStrapiMedia } from "utils/media";
-import { getGlobalData, getAllUsers } from "utils/api";
+import { getGlobalData } from "utils/api";
 import "@/styles/index.css";
 
 const MyApp = ({ Component, pageProps }) => {
